fc908de91831ac15f60f9bc37132e4b8d994d794
durzoflint <abhinavu1201@gmail.com>
2018-09-26 11:28:40 +0530

    Added Register and Login Functionality

--- app/src/main/java/durzoflint/mrpuncture/RegisterActivity.java
+++ app/src/main/java/durzoflint/mrpuncture/RegisterActivity.java
@@ -1,14 +1,13 @@
 package durzoflint.mrpuncture;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,24 +15,21 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class LoginActivity extends AppCompatActivity {
-
-    public static final String LOGIN_PREFS = "login_prefs";
-    public static final String USER_ID = "user_id";
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_regisrer);
 
         Button submit = findViewById(R.id.submit);
-
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                EditText name = findViewById(R.id.name);
                 EditText email = findViewById(R.id.email);
 
-                new Login().execute(email.getText().toString());
+                new Login().execute(name.getText().toString(), email.getText().toString());
             }
         });
     }
@@ -47,7 +43,7 @@ public class LoginActivity extends AppCompatActivity {
             URL url;
             HttpURLConnection urlConnection = null;
             try {
-                String myURL = baseUrl + "login.php?e=" + strings[0];
+                String myURL = baseUrl + "adduser.php?name=" + strings[0] + "&email=" + strings[1];
                 myURL = myURL.replaceAll(" ", "%20");
                 myURL = myURL.replaceAll("\'", "%27");
                 myURL = myURL.replaceAll("\'", "%22");
@@ -76,14 +72,11 @@ public class LoginActivity extends AppCompatActivity {
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
 
-            if (!webPage.isEmpty()) {
-                SharedPreferences sharedPreferences = getSharedPreferences(LOGIN_PREFS, Context.MODE_PRIVATE);
-                sharedPreferences.edit().putString(USER_ID, webPage).apply();
-
-                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
-                finish();
+            if (!webPage.equals("success")) {
+                Toast.makeText(RegisterActivity.this, "Some Error Occured while registering", Toast.LENGTH_SHORT).show();
             } else {
-                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                Toast.makeText(RegisterActivity.this, "Registration Success", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                 finish();
             }
         }
